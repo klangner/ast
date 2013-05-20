d4808354bd3ea615a5d07332b9bafd254a5bed87
Krzysztof Langner <klangner@gmail.com>
2013-05-20 22:24:37 +0200

    Updated spec

--- src/main/java/com/klangner/ast/IImport.java
+++ src/main/java/com/klangner/ast/IImport.java
@@ -2,4 +2,7 @@ package com.klangner.ast;
 
 
 public interface IImport extends INode{
+	
+	/** Get only packages name. E.g. without ending class */
+	public String getPackageName();
 }
--- src/main/java/com/klangner/ast/IModule.java
+++ src/main/java/com/klangner/ast/IModule.java
@@ -2,4 +2,7 @@ package com.klangner.ast;
 
 
 public interface IModule extends INode{
+	
+	/** Module name is the same as package where it resides */
+	public String getName();
 }
